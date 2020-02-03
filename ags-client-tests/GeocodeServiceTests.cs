5ffa651c58039460cf6065a90177a1f01fae627e
Andrew Nefiodovas <nef001@gmail.com>
2020-02-03 22:51:28 +0800

    Add GeocodeServiceTests Add test GeocodeServiceRequestTest

---
+++ ags-client-tests/GeocodeServiceTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using ags_client;
+using ags_client.Requests;
+using ags_client.Requests.GeocodeService;
+
+namespace ags_client_tests
+{
+    [TestClass]
+    public class GeocodeServiceTests
+    {
+        [TestMethod]
+        public void GeocodeServiceRequestTest()
+        {
+            // http://sampleserver6.arcgisonline.com/arcgis/rest/services/Locators/SanDiego/GeocodeServer
+
+            string host = "sampleserver6.arcgisonline.com";
+            string instance = "arcgis";
+            string folder = "Locators";
+            string geocodeServiceName = "SanDiego";
+
+
+            try
+            {
+                AgsClient agsClient = new AgsClient(host, instance, null, false, null, null);
+                var folderCatalog = new CatalogRequest(folder).Execute(agsClient);
+                Assert.IsNull(folderCatalog.error);
+                var geocodeService = new GeocodeServiceRequest(geocodeServiceName).Execute(agsClient, folderCatalog);
+                Assert.IsNull(geocodeService.error);
+                
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+    }
+}
